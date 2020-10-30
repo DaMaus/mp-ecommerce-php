8fbeccd3e082bf20b59e03ec4769afb40ac10320
mhernandez04 <mauricio.hernandez@figment.com.mx>
2020-10-29 20:41:54 -0600

    Ya jalooo

--- detail.php
+++ detail.php
@@ -149,14 +149,11 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    <!-- <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button> -->
                                     <script
                                         src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
                                         data-preference-id="<?php echo $preference->id; ?>">
-                                    </script>
-                                    <pre>
-                                        <?php var_dump($preference); ?>
-                                    </pre>                                       
+                                    </script>                                     
                                 </div>
                             </div>
                         </div>
