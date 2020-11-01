8ae53c8c704bc6f010eea9b515e929fdf45a92a6
mhernandez04 <mauricio.hernandez@figment.com.mx>
2020-11-01 15:57:24 -0600

    image10

--- detail.php
+++ detail.php
@@ -137,7 +137,7 @@
                                     <a href="<?= $preference->init_point; ?>">Pagar con Mercado Pago<a> 
                                     <pre>
                                     <?= ($preference->id); ?>
-                                    <?= print_r($preference->items->picture_url); ?>
+                                    <?= print_r($preference->items['picture_url']); ?>
                                     </pre>
                                                                      
                                 </div>
